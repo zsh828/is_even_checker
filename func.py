3871f7d8bbb527ef89303a4c764da36359e39b6d
zsh828 <1657477664@qq.com>
2026-05-05 14:30:39 +0800

    feat: update is_even function

---
+++ func.py
@@ -0,0 +1,11 @@
+def is_even(number: int) -> bool:
+    """
+    判断一个数字是否为偶数。
+    
+    Args:
+        number (int): 待判断的整数
+        
+    Returns:
+        bool: 如果是偶数返回 True，否则返回 False
+    """
+    return number % 2 == 0
