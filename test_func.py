7fae716bd67a0ee43db3d76f3ef0545d7295b088
zsh828 <1657477664@qq.com>
2026-05-05 15:43:52 +0800

    feat: add tests

--- test_func.py
+++ test_func.py
@@ -1,4 +1,6 @@
+# 测试代码
 from func import is_even
+import pytest
 
 
 class TestIsEven:
@@ -8,12 +10,14 @@ class TestIsEven:
         """测试正偶数"""
         assert is_even(2) is True
         assert is_even(4) is True
+        assert is_even(10) is True
         assert is_even(100) is True
 
     def test_negative_even(self):
         """测试负偶数"""
         assert is_even(-2) is True
         assert is_even(-4) is True
+        assert is_even(-10) is True
         assert is_even(-100) is True
 
     def test_zero(self):
@@ -24,15 +28,27 @@ class TestIsEven:
         """测试正奇数"""
         assert is_even(1) is False
         assert is_even(3) is False
-        assert is_even(99) is False
+        assert is_even(9) is False
+        assert is_even(101) is False
 
     def test_negative_odd(self):
         """测试负奇数"""
         assert is_even(-1) is False
         assert is_even(-3) is False
-        assert is_even(-99) is False
+        assert is_even(-9) is False
+        assert is_even(-101) is False
 
     def test_large_numbers(self):
         """测试大数"""
-        assert is_even(10**6) is True
-        assert is_even(10**6 + 1) is False
+        assert is_even(1000000) is True
+        assert is_even(1000001) is False
+        assert is_even(-1000000) is True
+        assert is_even(-1000001) is False
+
+    def test_single_digit_numbers(self):
+        """测试单个数字"""
+        for i in range(10):
+            if i % 2 == 0:
+                assert is_even(i) is True
+            else:
+                assert is_even(i) is False
